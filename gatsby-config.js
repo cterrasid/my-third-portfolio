7ebb2c57901f0482b7e0beba0a6158c7e2220141
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2021-11-28 11:38:25 +0100

    Connect with airtable to get the data of the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require("path");
 
 module.exports = {
@@ -31,6 +32,25 @@ module.exports = {
         Components: path.join(__dirname, "src", "components"),
         Containers: path.join(__dirname, "src", "containers"),
         Icons: path.join(__dirname, "src", "icons")
+      }
+    },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.AIRTABLE_KEY,
+        concurrency: 5,
+        tables: [
+          {
+            baseId: process.env.AIRTABLE_BASE_ID_ES,
+            tableName: `Header`,
+            separateNodeType: false,
+            separateMapType: false
+          },
+          {
+            baseId: process.env.AIRTABLE_BASE_ID_ES,
+            tableName: `Social Networks`
+          }
+        ]
       }
     }
   ]
